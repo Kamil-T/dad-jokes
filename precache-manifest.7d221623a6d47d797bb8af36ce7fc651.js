92b89100b1d755f825fbe97b95b14739827991d2
Kamil T <trzaskomakamil@gmail.com>
2019-09-23 15:14:12 +0200

    Updates

---
+++ precache-manifest.7d221623a6d47d797bb8af36ce7fc651.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f93cfea98932ce7a1564181d9459ffe",
+    "url": "/dad-jokes/index.html"
+  },
+  {
+    "revision": "dfa7d232c2971129efe6",
+    "url": "/dad-jokes/static/css/main.57760498.chunk.css"
+  },
+  {
+    "revision": "7411e169308c585505a3",
+    "url": "/dad-jokes/static/js/2.9acc90fe.chunk.js"
+  },
+  {
+    "revision": "dfa7d232c2971129efe6",
+    "url": "/dad-jokes/static/js/main.561c7bb9.chunk.js"
+  },
+  {
+    "revision": "fd7b546ce5490c6ebdd5",
+    "url": "/dad-jokes/static/js/runtime~main.f10d89c0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jokes/precache-manifest.bd88cd41c731621007d135d2e45e5011.js"
+  "/dad-jokes/precache-manifest.7d221623a6d47d797bb8af36ce7fc651.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jokes/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dad-jokes/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
